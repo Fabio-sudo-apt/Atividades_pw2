03572da6e53b8f3b36fcf60355ed03b573b6fefd
Fabio-sudo-apt <fsfreita1190@gmail.com>
2022-10-13 23:46:55 -0300

    add js

---
+++ toDolist/js/app.js
@@ -0,0 +1,21 @@
+const buttonTask = document.getElementById("add_tarefa");
+const task = document.getElementById("tarefa");
+const list = document.getElementById("a-fazer");
+const vazio = document.getElementsByClassName("lista-vazia")[0];
+let toDolist = [];
+
+buttonTask.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (list.classList.contains("lista-vazia")) {
+    list.removeChild(vazio);
+  }
+
+  const li = document.createElement("li");
+  const text = document.createTextNode(task.value);
+
+  li.appendChild(text);
+  list.appendChild(li);
+  toDolist.push(task.value);
+  task.value = "";
+});
