ea80a7dc227dc8a0c2aa59798f93443043bf4905
Fabio-sudo-apt <fsfreita1190@gmail.com>
2022-12-03 13:02:42 -0300

    tolist

--- toDolist/js/app.js
+++ toDolist/js/app.js
@@ -2,16 +2,14 @@ const buttonTask = document.getElementById("add_tarefa");
 const task = document.getElementById("tarefa");
 const list = document.getElementById("a-fazer");
 const vazio = document.getElementsByClassName("lista-vazia")[0];
+const feito = document.getElementById("feito")
 let toDolist = [];
 
 buttonTask.addEventListener("click", (event) => {
   event.preventDefault();
-
-  if (list.children[0].classList.contains("lista-vazia")) {
-    list.removeChild(vazio);
-  }
-
   const li = document.createElement("li");
+
+  li.setAttribute('onclick', 'itemClickFeito(this)')
   const text = document.createTextNode(task.value);
 
   li.appendChild(text);
@@ -19,3 +17,15 @@ buttonTask.addEventListener("click", (event) => {
   toDolist.push(task.value);
   task.value = "";
 });
+
+function itemClickFeito(e){
+  e.removeAttribute("onclick")
+  e.setAttribute('onclick', 'itemClickAfazer(this)')
+  feito.appendChild(e)
+}
+
+function itemClickAfazer(e){
+  e.removeAttribute("itemClickFeito")
+  e.setAttribute('onclick', 'itemClickFeito(this)')
+  list.appendChild(e)
+}
