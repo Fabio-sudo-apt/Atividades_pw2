bb48e493883e39ac65934750ca98af0519edfe7f
Fabio-sudo-apt <fsfreita1190@gmail.com>
2022-12-03 14:20:14 -0300

    twwiter

--- twitter/js/app.js
+++ twitter/js/app.js
@@ -7,16 +7,16 @@ const listPosts = document.getElementById("posts-list");
 const sideButton = document.querySelector(".sidebar__tweet");
 
 formButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    const tweet = newTwet.value;
-    createPost(tweet);
-    input.value = "";
-})
+  event.preventDefault();
+  const tweet = newTwet.value;
+  createPost(tweet);
+  newTwet.value = "";
+});
 
-function createPost(tweet){
-    const divMain = document.createElement("div")
-    divMain.setAttribute("class", "post")
-    const post = `
+function createPost(tweet) {
+  const divMain = document.createElement("div");
+  divMain.setAttribute("class", "post");
+  const post = `
         <div class="post__avatar">
           <img
             src="https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png"
@@ -34,23 +34,50 @@ function createPost(tweet){
                 >
               </h3>
             </div>
-            <div class="post__headerDescription">
+            <div class="post__headerDescription" id="post__headerD">
               <p>${tweet}</p>
             </div>
           </div>
           <div class="post__footer">
-            <span class="material-icons"> 
+            <span class="material-icons" onclick="editItem(this)"> 
                 <i class="fa-solid fa-pen-to-square"></i>
             </span>
             <span class="material-icons"> 
                 <i class="fa-solid fa-heart"></i>
             </span>
-            <span class="material-icons"> 
-                <i class="fa-solid fa-trash"></i>
+            <span class="material-icons" onclick="deleteItem(this)"> 
+                <i class="fa-solid fa-trash" ></i>
              </span>
           </div>
         </div>
     `;
-    divMain.innerHTML = post
-    listPosts.appendChild(divMain);
+  divMain.innerHTML = post;
+  listPosts.appendChild(divMain);
+}
+
+function editItem(e) {
+  const input = document.createElement("input");
+  const button = document.createElement("input");
+  input.setAttribute("id", "IdInput");
+  button.setAttribute("type", "button");
+  button.setAttribute("value", "Editar");
+
+  const post__headerDescription = document.getElementById("post__headerD");
+  post__headerDescription.removeChild(post__headerDescription.children[0]);
+  post__headerDescription.appendChild(input);
+  post__headerDescription.appendChild(button);
+
+  button.addEventListener("click", () => {
+    let valueInput = document.getElementById("IdInput");
+    const pg = document.createElement("p");
+    pg.innerHTML = valueInput.value
+    document.getElementById("post__headerD").removeChild(button)
+    document.getElementById("post__headerD").removeChild(input)
+    post__headerDescription.appendChild(pg);
+  });
+}
+
+function deleteItem(e) {
+  let post = e.parentNode.parentNode.parentNode;
+  listPosts.removeChild(post);
 }
